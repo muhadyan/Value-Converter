d62aa96a567c639ecc54ba95d7945fb375c8782b
m-adyan <madyanr@gmail.com>
2022-01-07 23:25:44 +0700

    revisi converter

--- converter.js
+++ converter.js
@@ -1,15 +1,13 @@
 let nilai = prompt("Input niilai: ")
 
-if (nilai >= 80 && nilai <= 100) {
+if (nilai >= 80) {
     alert("Nilai anda A")
-} else if (nilai >= 60 && nilai < 80) {
+} else if (nilai >= 60) {
     alert("Nilai anda B")
-}else if (nilai >= 40 && nilai < 60) {
+}else if (nilai >= 40) {
     alert("Nilai anda C")
-}else if (nilai >= 20 && nilai < 40) {
+}else if (nilai >= 20) {
     alert("Nilai anda D")
-}else if (nilai >= 0 && nilai < 20) {
-    alert("Nilai anda E")
 }else {
-    alert("Nilai yang anda masukan tidak sesuai")
+    alert("Nilai anda E")
 }
